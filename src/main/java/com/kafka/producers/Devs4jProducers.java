cf6ec3839cc202ed4fd4b6e1f76c1adca3c73e55
Cesar Estiven Medrano <cesar-2007-mesa@hotmail.com>
2023-07-26 23:57:41 -0500

    Sync & Async

--- src/main/java/com/kafka/producers/Devs4jProducers.java
+++ src/main/java/com/kafka/producers/Devs4jProducers.java
@@ -1,14 +1,35 @@
 package com.kafka.producers;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class Devs4jProducers {
 
-    public static void main(String[] args) {
+    static Logger logger = LoggerFactory.getLogger(Devs4jProducers.class);
+    public static void main(String[] args)  {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("acks", "1");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserialize", "\"org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("bootstrap.servers", "localhost:9092"); //Broker kafka
+        props.put("acks", "1"); //performance
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer"); //Codificación
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        try(Producer<String, String> producer = new KafkaProducer<>(props)){
+            for(int i = 0; i < 10000; i++){
+                //Sin el get Asyncrona
+                producer.send(new ProducerRecord<String, String>("devs4j-topic", String.valueOf(i), "devs4j-value")).get();
+            }
+            producer.flush();
+        }
+        //Syncrona
+        catch (ExecutionException  | InterruptedException e) {
+            logger.error("Message interruped", e);
+        }
+
     }
 }
